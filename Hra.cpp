f31e3995f15754f535359aa37b68c6926f82a88e
micha1puk1 <michal.pukl.009@gmail.com>
2026-04-16 15:55:46 +0200

    Vytvoření vybýrání postav

--- Hra.cpp
+++ Hra.cpp
@@ -1,1 +1,32 @@
 #include <iostream>
+using namespace std;
+
+int main() {
+
+  int pocetpostav = 4;
+  string postava[pocetpostav] = {"Revizor", "Bezdomovec", "Černý pasažér", "Ajťák"};
+  int cislopostavy;
+
+  cout <<"Vybete si postavu:";
+
+ do {
+     cout << "\n1. Revizor";
+     cout << "\n2. Bezdomovec";
+     cout << "\n3. Černý pasažér";
+     cout << "\n4. Ajťák";
+     cout << "\nČíslo postavy: ";
+     cin >> cislopostavy;
+ }while (cislopostavy <= 0 || cislopostavy > 4);
+
+  cout << endl << postava[cislopostavy-1];
+
+
+
+
+
+
+
+
+
+
+}
