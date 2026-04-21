747798766a7cf87e1494bf969df80f776f114071
micha1puk1 <michal.pukl.009@gmail.com>
2026-04-21 16:10:13 +0200

    Vybýrání postav, fungující diakritika

--- Hra.cpp
+++ Hra.cpp
@@ -1,33 +1,65 @@
 #include <iostream>
+#include <windows.h>
 using namespace std;
 
+void vyberPostav() {
+    cout <<"\nVybete si postavu:";
+    cout << "\n1. Revizor";
+    cout << "\n2. Bezdomovec";
+    cout << "\n3. Černý pasažér";
+    cout << "\n4. Ajťák";
+    cout << "\nČíslo postavy: ";
+}
+
+void revizorInfo() {
+    cout << "Popis postavy Revizor";
+}
+void bezdomovecInfo() {
+    cout << "Popis postavy Bezdomovec";
+}
+void cernypasazerInfo() {
+    cout << "Popis postavy Cernypasazer";
+}
+
+void ajtakInfo() {
+    cout << "Popis postavy Ajtak";
+}
+
 int main() {
+    SetConsoleOutputCP(CP_UTF8);
 
+    int pocetpostav = 4;
+    string postava[pocetpostav] = {"Revizor", "Bezdomovec", "Černý pasažér", "Ajťák"};
+    int cislopostavy;
+    char vyberanone;
 
-  int pocetpostav = 4;
-  string postava[pocetpostav] = {"Revizor", "Bezdomovec", "Černý pasažér", "Ajťák"};
-  int cislopostavy;
+    cout << "Právě se nacházíte ve vlaku v posledním vagónu. \nVaše mise je se dostat do prvního vagonu. \nPotom přelézt do lokomotivy. \nZneškodnit strojvedoucího a ovládnout celý vlak.\n";
 
-  cout <<"Vybete si postavu:";
+    do {
+        vyberPostav();
+        cin >> cislopostavy;
+        while (cislopostavy <= 0 || cislopostavy > 4){
+            cout << "\nNEPLATNÝ ÚDAJ\n";
+            vyberPostav();
+            cin >> cislopostavy;
+        }
 
- do {
-     cout << "\n1. Revizor";
-     cout << "\n2. Bezdomovec";
-     cout << "\n3. Černý pasažér";
-     cout << "\n4. Ajťák";
-     cout << "\nČíslo postavy: ";
-     cin >> cislopostavy;
- }while (cislopostavy <= 0 || cislopostavy > 4);
+        cout << endl << postava[cislopostavy-1]<<endl;
+        switch (cislopostavy) {
+            case 1: revizorInfo(); break;
+            case 2: bezdomovecInfo(); break;
+            case 3: cernypasazerInfo(); break;
+            case 4: ajtakInfo(); break;
+            default: cout << "ERROR";
+        }
 
-  cout << endl << postava[cislopostavy-1];
+        do {
+            cout << "\nChcete vybrat tuto postavu (a/n): ";
+            cin  >> vyberanone;
+        }while ((vyberanone != 'a') && (vyberanone != 'n'));
 
+    }while (vyberanone == 'n');
 
-
-
-
-
-
-
-
+    cout << "Konec";
 
 }
